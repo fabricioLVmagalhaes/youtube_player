7d23433006cebabf57d6ef6f0b7823b948460d46
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-11-23 21:02:07 -0300

    onInitializationSuccess Method

--- app/src/main/java/com/magalhaes/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/com/magalhaes/youtubeplayer/YoutubeActivity.kt
@@ -1,6 +1,7 @@
 package com.magalhaes.youtubeplayer
 
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
@@ -10,10 +11,12 @@ import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerView
 
-    const val  YOUTUBE_VIDEO_ID = "watch?v=9VoYpnrD6FM&list=RDEMFrom7aEgbUpjlEDaTkXmHA&index=5"
+    const val  YOUTUBE_VIDEO_ID = "wFjjchZGg0w"
     const val  YOUTUBE_PLAYLIST = "playlist?list=PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj"
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+
+    private val TAG = "youtubeactivity"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,11 +40,16 @@ class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListen
     }
 
     override fun onInitializationSuccess(
-        p0: YouTubePlayer.Provider?,
-        p1: YouTubePlayer?,
-        p2: Boolean
+        provider: YouTubePlayer.Provider?,
+        youtubePlayer: YouTubePlayer?,
+        wasRestored: Boolean
     ) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d(TAG, "onInitializationSuccess: provider is ${provider?.javaClass}")
+        Log.d(TAG, "onInitializationSuccess: youtubePlayer is ${youtubePlayer?.javaClass}")
+        Toast.makeText(this, "Initialized Youtube Player successfully", Toast.LENGTH_SHORT).show()
+        if(!wasRestored) {
+            youtubePlayer?.cueVideo(YOUTUBE_VIDEO_ID)
+        }
     }
 
     override fun onInitializationFailure(
